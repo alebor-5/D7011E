6b324f1f141ce8daa07183a7daf235148cac6fbb
alebor-5 <alebor-5@student.ltu.se>
2018-11-01 23:36:42 +0100

    added test cases for opt

--- present/opt.s
+++ present/opt.s
@@ -22,47 +22,82 @@ sw $at, 0($sp)
 Label_8: lw $at, 0($sp) 
 addi $sp, $sp, 4 
 sw $at, 8($gp) 
-Label_9: lw $at, 4($gp) 
+Label_9: addi $at, $zero, 1
 addi $sp, $sp, -4 
 sw $at, 0($sp)
-Label_10: addi $at, $zero, 2
-addi $sp, $sp, -4 
-sw $at, 0($sp)
-Label_11: lw $s0, 0($sp) 
-
-  lw $s1, 4($sp) 
-
-  addu $s0, $s0, $s1 
-
-  addi $sp, $sp, 4
-
-  sw $s0, 0($sp)
-Label_12: lw $at, 4($gp) 
-addi $sp, $sp, -4 
-sw $at, 0($sp)
-Label_13: addi $at, $zero, 2
-addi $sp, $sp, -4 
-sw $at, 0($sp)
-Label_14: lw $s0, 0($sp) 
-
-  lw $s1, 4($sp) 
-
-  addu $s0, $s0, $s1 
-
-  addi $sp, $sp, 4
-
-  sw $s0, 0($sp)
-Label_15: lw $s0, 0($sp) 
-
-  lw $s1, 4($sp) 
-
-  addu $s0, $s0, $s1 
-
-  addi $sp, $sp, 4
-
-  sw $s0, 0($sp)
-Label_16: lw $at, 0($sp) 
+Label_10: lw $at, 0($sp) 
 addi $sp, $sp, 4 
-sw $at, 12($gp) 
-Label_17: j Label_17
+sw $at, 4($gp) 
+Label_11: addi $at, $zero, 0
+addi $sp, $sp, -4 
+sw $at, 0($sp)
+Label_12: lw $at, 0($sp) 
+addi $sp, $sp, 4 
+sw $at, 4($gp) 
+Label_13: addi $at, $zero, 1
+addi $sp, $sp, -4 
+sw $at, 0($sp)
+Label_14: lw $at, 0($sp) 
+addi $sp, $sp, 4 
+sw $at, 4($gp) 
+Label_15: lw $at, 4($gp) 
+addi $sp, $sp, -4 
+sw $at, 0($sp)
+Label_16: addi $at, $zero, 12
+addi $sp, $sp, -4 
+sw $at, 0($sp)
+Label_17: lw $s0, 0($sp) 
+
+  lw $s1, 4($sp) 
+
+  addi $sp, $sp, 8 
+
+  bne $s1, $s0, Label_21
+Label_18: addi $at, $zero, 0
+addi $sp, $sp, -4 
+sw $at, 0($sp)
+Label_19: lw $at, 0($sp) 
+addi $sp, $sp, 4 
+sw $at, 4($gp) 
+Label_20: j Label_23
+Label_21: addi $at, $zero, 1
+addi $sp, $sp, -4 
+sw $at, 0($sp)
+Label_22: lw $at, 0($sp) 
+addi $sp, $sp, 4 
+sw $at, 4($gp) 
+Label_23: lw $at, 4($gp) 
+addi $sp, $sp, -4 
+sw $at, 0($sp)
+Label_24: addi $at, $zero, 12
+addi $sp, $sp, -4 
+sw $at, 0($sp)
+Label_25: lw $s0, 0($sp) 
+
+  lw $s1, 4($sp) 
+
+  addi $sp, $sp, 8 
+
+  bne $s1, $s0, Label_29
+Label_26: addi $at, $zero, 0
+addi $sp, $sp, -4 
+sw $at, 0($sp)
+Label_27: lw $at, 0($sp) 
+addi $sp, $sp, 4 
+sw $at, 4($gp) 
+Label_28: j Label_31
+Label_29: addi $at, $zero, 1
+addi $sp, $sp, -4 
+sw $at, 0($sp)
+Label_30: lw $at, 0($sp) 
+addi $sp, $sp, 4 
+sw $at, 4($gp) 
+Label_31: addi $at, $zero, 1
+addi $sp, $sp, -4 
+sw $at, 0($sp)
+Label_32: lw $at, 0($sp) 
+addi $sp, $sp, 4 
+sw $at, 4($gp) 
+Label_33: j Label_31
+Label_34: j Label_34
 
